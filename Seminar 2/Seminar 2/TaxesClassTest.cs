03af7f44d4e7c002f344b5f2c33bacbd111f205a
Kevin DeWire <kevind4272@yahoo.com>
2016-05-21 21:36:56 -0400

    Contains all four filing status.
    Also changed the name of the test class.

---
+++ Seminar 2/Seminar 2/TaxesClassTest.cs
@@ -0,0 +1,356 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TaxesClass;
+
+namespace Seminar_2
+{
+    [TestClass]
+    public class TaxesClassTest
+    {
+        [TestMethod]
+        public void Single_0()
+        {
+            Taxes taxes = new Taxes();
+            double income = 0;
+            double expectedTax = 0;
+            double tax = taxes.Single(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+
+        [TestMethod]
+        public void Single_NegativeNumber()
+        {
+            Taxes taxes = new Taxes();
+            double income = -1000;
+            double expectedTax = 0;
+            double tax = taxes.Single(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+
+        [TestMethod]
+        public void Single_5000()
+        {
+            Taxes taxes = new Taxes();
+            double income = 5000;
+            double expectedTax = 500;
+            double tax = taxes.Single(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+        [TestMethod]
+        public void Single_9075()
+        {
+            Taxes taxes = new Taxes();
+            double income = 9075;
+            double expectedTax = 907.5;
+            double tax = taxes.Single(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+        [TestMethod]
+        public void Single_36900()
+        {
+            Taxes taxes = new Taxes();
+            double income = 36900;
+            double expectedTax = 5081.25;
+            double tax = taxes.Single(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+        [TestMethod]
+        public void Single_20000()
+        {
+            Taxes taxes = new Taxes();
+            double income = 20000;
+            double expectedTax = 2546.25;
+            double tax = taxes.Single(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+
+        [TestMethod]
+        public void Single_89350()
+        {
+            Taxes taxes = new Taxes();
+            double income = 89350;
+            double expectedTax = 18193.75;
+            double tax = taxes.Single(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+
+        [TestMethod]
+        public void Single_186350()
+        {
+            Taxes taxes = new Taxes();
+            double income = 186350;
+            double expectedTax = 45353.75;
+            double tax = taxes.Single(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+
+        [TestMethod]
+        public void Single_405100()
+        {
+            Taxes taxes = new Taxes();
+            double income = 405100;
+            double expectedTax = 117541.25;
+            double tax = taxes.Single(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+
+        [TestMethod]
+        public void Single_406750()
+        {
+            Taxes taxes = new Taxes();
+            double income = 406750;
+            double expectedTax = 118118.75;
+            double tax = taxes.Single(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+
+        [TestMethod]
+        public void Single_500000()
+        {
+            Taxes taxes = new Taxes();
+            double income = 500000;
+            double expectedTax = 155045.75;
+            double tax = taxes.Single(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+
+        [TestMethod]
+        public void Jointly_0()
+        {
+            Taxes taxes = new Taxes();
+            double income = 0;
+            double expectedTax = 0;
+            double tax = taxes.Jointly(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+
+        [TestMethod]
+        public void Jointly_18150()
+        {
+            Taxes taxes = new Taxes();
+            double income = 18150;
+            double expectedTax = 1815.00;
+            double tax = taxes.Jointly(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+
+        [TestMethod]
+        public void Jointly_73800()
+        {
+            Taxes taxes = new Taxes();
+            double income = 73800;
+            double expectedTax = 10162.50;
+            double tax = taxes.Jointly(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+
+        [TestMethod]
+        public void Jointly_148850()
+        {
+            Taxes taxes = new Taxes();
+            double income = 148850;
+            double expectedTax = 28925.00;
+            double tax = taxes.Jointly(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+
+        [TestMethod]
+        public void Jointly_226850()
+        {
+            Taxes taxes = new Taxes();
+            double income = 226850;
+            double expectedTax = 50765.00;
+            double tax = taxes.Jointly(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+
+        [TestMethod]
+        public void Jointly_405100()
+        {
+            Taxes taxes = new Taxes();
+            double income = 405100;
+            double expectedTax = 109587.50;
+            double tax = taxes.Jointly(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+
+        [TestMethod]
+        public void Jointly_457600()
+        {
+            Taxes taxes = new Taxes();
+            double income = 457600;
+            double expectedTax = 127962.50;
+            double tax = taxes.Jointly(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+
+        [TestMethod]
+        public void Jointly_500000()
+        {
+            Taxes taxes = new Taxes();
+            double income = 500000;
+            double expectedTax = 144752.9;
+            double tax = taxes.Jointly(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+
+        [TestMethod]
+        public void Separately_0()
+        {
+            Taxes taxes = new Taxes();
+            double income = 0;
+            double expectedTax = 0;
+            double tax = taxes.Separately(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+
+        [TestMethod]
+        public void Separately_9075()
+        {
+            Taxes taxes = new Taxes();
+            double income = 9075;
+            double expectedTax = 907.50;
+            double tax = taxes.Separately(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+
+        [TestMethod]
+        public void Separately_36900()
+        {
+            Taxes taxes = new Taxes();
+            double income = 36900;
+            double expectedTax = 5081.25;
+            double tax = taxes.Separately(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+
+        [TestMethod]
+        public void Separately_74425()
+        {
+            Taxes taxes = new Taxes();
+            double income = 74425;
+            double expectedTax = 14462.50;
+            double tax = taxes.Separately(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+
+        [TestMethod]
+        public void Separately_113425()
+        {
+            Taxes taxes = new Taxes();
+            double income = 113425;
+            double expectedTax = 25382.50;
+            double tax = taxes.Separately(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+
+        [TestMethod]
+        public void Separately_202550()
+        {
+            Taxes taxes = new Taxes();
+            double income = 202550;
+            double expectedTax = 54793.75;
+            double tax = taxes.Separately(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+
+        [TestMethod]
+        public void Separately_228800()
+        {
+            Taxes taxes = new Taxes();
+            double income = 228800;
+            double expectedTax = 63981.25;
+            double tax = taxes.Separately(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+
+        [TestMethod]
+        public void Separately_500000()
+        {
+            Taxes taxes = new Taxes();
+            double income = 500000;
+            double expectedTax = 171376.45;
+            double tax = taxes.Separately(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+
+        [TestMethod]
+        public void Head_0()
+        {
+            Taxes taxes = new Taxes();
+            double income = 0;
+            double expectedTax = 0;
+            double tax = taxes.Head(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+
+        [TestMethod]
+        public void Head_12950()
+        {
+            Taxes taxes = new Taxes();
+            double income = 12950;
+            double expectedTax = 1295.00;
+            double tax = taxes.Head(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+
+        [TestMethod]
+        public void Head_49400()
+        {
+            Taxes taxes = new Taxes();
+            double income = 49400;
+            double expectedTax = 6762.50;
+            double tax = taxes.Head(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+
+        [TestMethod]
+        public void Head_127550()
+        {
+            Taxes taxes = new Taxes();
+            double income = 127550;
+            double expectedTax = 26300.00;
+            double tax = taxes.Head(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+
+        [TestMethod]
+        public void Head_206600()
+        {
+            Taxes taxes = new Taxes();
+            double income = 206600;
+            double expectedTax = 48434.00;
+            double tax = taxes.Head(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+
+        [TestMethod]
+        public void Head_405100()
+        {
+            Taxes taxes = new Taxes();
+            double income = 405100;
+            double expectedTax = 113939.00;
+            double tax = taxes.Head(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+
+        [TestMethod]
+        public void Head_432200()
+        {
+            Taxes taxes = new Taxes();
+            double income = 432200;
+            double expectedTax = 123424.00;
+            double tax = taxes.Head(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+
+        [TestMethod]
+        public void Head_500000()
+        {
+            Taxes taxes = new Taxes();
+            double income = 500000;
+            double expectedTax = 150272.8;
+            double tax = taxes.Head(income);
+            Assert.AreEqual(expectedTax, tax, 0.00);
+        }
+    }
+}
--- Seminar 2/Taxes/Taxes.cs
+++ Seminar 2/Taxes/Taxes.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TaxesClass
+﻿namespace TaxesClass
 {
     public class Taxes
     {
@@ -16,75 +10,77 @@ namespace TaxesClass
             double taxRate;
             double baseTax;
 
-
-            if (income <= 9075)
+            if (income > 0)
             {
-                taxRate = 0.1;
-                incomeDeduction = 0;
-                taxableIncome = income - incomeDeduction;
-                baseTax = 0;
-                tax = baseTax + taxableIncome * taxRate;
-                return tax;
-            }
+                if (income <= 9075)
+                {
+                    taxRate = 0.1;
+                    incomeDeduction = 0;
+                    taxableIncome = income - incomeDeduction;
+                    baseTax = 0;
+                    tax = baseTax + taxableIncome * taxRate;
+                    return tax;
+                }
 
-            if (income <= 36900)
-            {
-                taxRate = 0.15;
-                incomeDeduction = 9075;
-                taxableIncome = income - incomeDeduction;
-                baseTax = 907.5;
-                tax = baseTax + taxableIncome * taxRate;
-                return tax;
-            }
+                if (income <= 36900)
+                {
+                    taxRate = 0.15;
+                    incomeDeduction = 9075;
+                    taxableIncome = income - incomeDeduction;
+                    baseTax = 907.5;
+                    tax = baseTax + taxableIncome * taxRate;
+                    return tax;
+                }
 
-            if (income <= 89350)
-            {
-                taxRate = 0.25;
-                incomeDeduction = 36900;
-                taxableIncome = income - incomeDeduction;
-                baseTax = 5081.25;
-                tax = baseTax + taxableIncome * taxRate;
-                return tax;
-            }
+                if (income <= 89350)
+                {
+                    taxRate = 0.25;
+                    incomeDeduction = 36900;
+                    taxableIncome = income - incomeDeduction;
+                    baseTax = 5081.25;
+                    tax = baseTax + taxableIncome * taxRate;
+                    return tax;
+                }
 
-            if (income <= 186350)
-            {
-                taxRate = 0.28;
-                incomeDeduction = 89350;
-                taxableIncome = income - incomeDeduction;
-                baseTax = 18193.75;
-                tax = baseTax + taxableIncome * taxRate;
-                return tax;
-            }
+                if (income <= 186350)
+                {
+                    taxRate = 0.28;
+                    incomeDeduction = 89350;
+                    taxableIncome = income - incomeDeduction;
+                    baseTax = 18193.75;
+                    tax = baseTax + taxableIncome * taxRate;
+                    return tax;
+                }
 
-            if (income <= 405100)
-            {
-                taxRate = 0.33;
-                incomeDeduction = 186350;
-                taxableIncome = income - incomeDeduction;
-                baseTax = 45353.75;
-                tax = baseTax + taxableIncome * taxRate;
-                return tax;
-            }
+                if (income <= 405100)
+                {
+                    taxRate = 0.33;
+                    incomeDeduction = 186350;
+                    taxableIncome = income - incomeDeduction;
+                    baseTax = 45353.75;
+                    tax = baseTax + taxableIncome * taxRate;
+                    return tax;
+                }
 
-            if (income <= 406750)
-            {
-                taxRate = 0.35;
-                incomeDeduction = 405100;
-                taxableIncome = income - incomeDeduction;
-                baseTax = 117541.25;
-                tax = baseTax + taxableIncome * taxRate;
-                return tax;
-            }
+                if (income <= 406750)
+                {
+                    taxRate = 0.35;
+                    incomeDeduction = 405100;
+                    taxableIncome = income - incomeDeduction;
+                    baseTax = 117541.25;
+                    tax = baseTax + taxableIncome * taxRate;
+                    return tax;
+                }
 
-            if (income > 406750)
-            {
-                taxRate = 0.396;
-                incomeDeduction = 406750;
-                taxableIncome = income - incomeDeduction;
-                baseTax = 118118.75;
-                tax = baseTax + taxableIncome * taxRate;
-                return tax;
+                if (income > 406750)
+                {
+                    taxRate = 0.396;
+                    incomeDeduction = 406750;
+                    taxableIncome = income - incomeDeduction;
+                    baseTax = 118118.75;
+                    tax = baseTax + taxableIncome * taxRate;
+                    return tax;
+                }
             }
 
             return tax;
@@ -98,75 +94,245 @@ namespace TaxesClass
             double taxRate;
             double baseTax;
 
-
-            if (income <= 18150)
+            if (income > 0)
             {
-                taxRate = 0.1;
-                incomeDeduction = 0;
-                taxableIncome = income - incomeDeduction;
-                baseTax = 0;
-                tax = baseTax + taxableIncome * taxRate;
-                return tax;
+                if (income <= 18150)
+                {
+                    taxRate = 0.1;
+                    incomeDeduction = 0;
+                    taxableIncome = income - incomeDeduction;
+                    baseTax = 0;
+                    tax = baseTax + taxableIncome * taxRate;
+                    return tax;
+                }
+
+                if (income <= 73800)
+                {
+                    taxRate = 0.15;
+                    incomeDeduction = 18150;
+                    taxableIncome = income - incomeDeduction;
+                    baseTax = 1815.00;
+                    tax = baseTax + taxableIncome * taxRate;
+                    return tax;
+                }
+
+                if (income <= 148850)
+                {
+                    taxRate = 0.25;
+                    incomeDeduction = 73800;
+                    taxableIncome = income - incomeDeduction;
+                    baseTax = 10162.50;
+                    tax = baseTax + taxableIncome * taxRate;
+                    return tax;
+                }
+
+                if (income <= 226850)
+                {
+                    taxRate = 0.28;
+                    incomeDeduction = 148850;
+                    taxableIncome = income - incomeDeduction;
+                    baseTax = 28925.00;
+                    tax = baseTax + taxableIncome * taxRate;
+                    return tax;
+                }
+
+                if (income <= 405100)
+                {
+                    taxRate = 0.33;
+                    incomeDeduction = 226850;
+                    taxableIncome = income - incomeDeduction;
+                    baseTax = 50765.00;
+                    tax = baseTax + taxableIncome * taxRate;
+                    return tax;
+                }
+
+                if (income <= 457600)
+                {
+                    taxRate = 0.35;
+                    incomeDeduction = 405100;
+                    taxableIncome = income - incomeDeduction;
+                    baseTax = 109587.50;
+                    tax = baseTax + taxableIncome * taxRate;
+                    return tax;
+                }
+
+                if (income > 457600)
+                {
+                    taxRate = 0.396;
+                    incomeDeduction = 457600;
+                    taxableIncome = income - incomeDeduction;
+                    baseTax = 127962.50;
+                    tax = baseTax + taxableIncome * taxRate;
+                    return tax;
+                }
             }
 
-            if (income <= 73800)
+            return tax;
+        }
+
+        public double Separately(double income)
+        {
+            double tax = 0;
+            double taxableIncome;
+            double incomeDeduction;
+            double taxRate;
+            double baseTax;
+
+            if (income > 0)
             {
-                taxRate = 0.15;
-                incomeDeduction = 18150;
-                taxableIncome = income - incomeDeduction;
-                baseTax = 1815.00;
-                tax = baseTax + taxableIncome * taxRate;
-                return tax;
+                if (income <= 9075)
+                {
+                    taxRate = 0.1;
+                    incomeDeduction = 0;
+                    taxableIncome = income - incomeDeduction;
+                    baseTax = 0;
+                    tax = baseTax + taxableIncome * taxRate;
+                    return tax;
+                }
+
+                if (income <= 36900)
+                {
+                    taxRate = 0.15;
+                    incomeDeduction = 9075;
+                    taxableIncome = income - incomeDeduction;
+                    baseTax = 907.50;
+                    tax = baseTax + taxableIncome * taxRate;
+                    return tax;
+                }
+
+                if (income <= 74425)
+                {
+                    taxRate = 0.25;
+                    incomeDeduction = 36900;
+                    taxableIncome = income - incomeDeduction;
+                    baseTax = 5081.25;
+                    tax = baseTax + taxableIncome * taxRate;
+                    return tax;
+                }
+
+                if (income <= 113425)
+                {
+                    taxRate = 0.28;
+                    incomeDeduction = 74425;
+                    taxableIncome = income - incomeDeduction;
+                    baseTax = 14462.50;
+                    tax = baseTax + taxableIncome * taxRate;
+                    return tax;
+                }
+
+                if (income <= 202550)
+                {
+                    taxRate = 0.33;
+                    incomeDeduction = 113425;
+                    taxableIncome = income - incomeDeduction;
+                    baseTax = 25382.50;
+                    tax = baseTax + taxableIncome * taxRate;
+                    return tax;
+                }
+
+                if (income <= 228800)
+                {
+                    taxRate = 0.35;
+                    incomeDeduction = 202550;
+                    taxableIncome = income - incomeDeduction;
+                    baseTax = 54793.75;
+                    tax = baseTax + taxableIncome * taxRate;
+                    return tax;
+                }
+
+                if (income > 228800)
+                {
+                    taxRate = 0.396;
+                    incomeDeduction = 228800;
+                    taxableIncome = income - incomeDeduction;
+                    baseTax = 63981.25;
+                    tax = baseTax + taxableIncome * taxRate;
+                    return tax;
+                }
             }
 
-            if (income <= 148850)
-            {
-                taxRate = 0.25;
-                incomeDeduction = 73800;
-                taxableIncome = income - incomeDeduction;
-                baseTax = 10162.50;
-                tax = baseTax + taxableIncome * taxRate;
-                return tax;
-            }
+            return tax;
+        }
 
-            if (income <= 226850)
-            {
-                taxRate = 0.28;
-                incomeDeduction = 148850;
-                taxableIncome = income - incomeDeduction;
-                baseTax = 28925.00;
-                tax = baseTax + taxableIncome * taxRate;
-                return tax;
-            }
+        public double Head(double income)
+        {
+            double tax = 0;
+            double taxableIncome;
+            double incomeDeduction;
+            double taxRate;
+            double baseTax;
 
-            if (income <= 405100)
+            if (income > 0)
             {
-                taxRate = 0.33;
-                incomeDeduction = 226850;
-                taxableIncome = income - incomeDeduction;
-                baseTax = 50765.00;
-                tax = baseTax + taxableIncome * taxRate;
-                return tax;
-            }
+                if (income <= 12950)
+                {
+                    taxRate = 0.1;
+                    incomeDeduction = 0;
+                    taxableIncome = income - incomeDeduction;
+                    baseTax = 0;
+                    tax = baseTax + taxableIncome * taxRate;
+                    return tax;
+                }
 
-            if (income <= 457600)
-            {
-                taxRate = 0.35;
-                incomeDeduction = 405100;
-                taxableIncome = income - incomeDeduction;
-                baseTax = 109587.50;
-                tax = baseTax + taxableIncome * taxRate;
-                return tax;
-            }
+                if (income <= 49400)
+                {
+                    taxRate = 0.15;
+                    incomeDeduction = 12950;
+                    taxableIncome = income - incomeDeduction;
+                    baseTax = 1295.00;
+                    tax = baseTax + taxableIncome * taxRate;
+                    return tax;
+                }
 
-            if (income > 457600)
-            {
-                taxRate = 0.396;
-                incomeDeduction = 457600;
-                taxableIncome = income - incomeDeduction;
-                baseTax = 127962.50;
-                tax = baseTax + taxableIncome * taxRate;
-                return tax;
+                if (income <= 127550)
+                {
+                    taxRate = 0.25;
+                    incomeDeduction = 49400;
+                    taxableIncome = income - incomeDeduction;
+                    baseTax = 6762.50;
+                    tax = baseTax + taxableIncome * taxRate;
+                    return tax;
+                }
+
+                if (income <= 206600)
+                {
+                    taxRate = 0.28;
+                    incomeDeduction = 127550;
+                    taxableIncome = income - incomeDeduction;
+                    baseTax = 26300.00;
+                    tax = baseTax + taxableIncome * taxRate;
+                    return tax;
+                }
+
+                if (income <= 405100)
+                {
+                    taxRate = 0.33;
+                    incomeDeduction = 206600;
+                    taxableIncome = income - incomeDeduction;
+                    baseTax = 48434.00;
+                    tax = baseTax + taxableIncome * taxRate;
+                    return tax;
+                }
+
+                if (income <= 432200)
+                {
+                    taxRate = 0.35;
+                    incomeDeduction = 405100;
+                    taxableIncome = income - incomeDeduction;
+                    baseTax = 113939.00;
+                    tax = baseTax + taxableIncome * taxRate;
+                    return tax;
+                }
+
+                if (income > 432200)
+                {
+                    taxRate = 0.396;
+                    incomeDeduction = 432200;
+                    taxableIncome = income - incomeDeduction;
+                    baseTax = 123424.00;
+                    tax = baseTax + taxableIncome * taxRate;
+                    return tax;
+                }
             }
 
             return tax;
