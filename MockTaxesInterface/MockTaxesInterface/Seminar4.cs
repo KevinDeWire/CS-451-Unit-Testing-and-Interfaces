8de3e65308a9120308357b89e85b9c3bbd166afb
Kevin DeWire <kevind4272@yahoo.com>
2016-06-06 19:00:09 -0400

    Seminar 4 Assignment
    Created a class that uses an interface to access
    the information from the previous weeks Taxes class.
    
    Also created a Mock of the Taxes class to test the
    new class without relying on dependancies with the
    original class.

---
+++ MockTaxesInterface/MockTaxesInterface/Seminar4.cs
@@ -0,0 +1,37 @@
+﻿namespace MockTaxesInterface
+{
+    public interface ITaxes
+    {
+        double Single(double Income);
+        double MarriedFilingJointly(double Income);
+        double MarriedFilingSeperately(double Income);
+        double HeadOfHousehold(double Income);
+    }
+
+    public class TaxDetermination
+    {
+       private readonly ITaxes _taxes;
+
+       public TaxDetermination(ITaxes taxes)
+        {
+            _taxes = taxes;
+        }
+
+       public double ScheduleSort(string Schedule, double Income)
+        {
+            switch (Schedule.ToUpper())
+            {
+                case "X":
+                    return _taxes.Single(Income);
+                case "Y1":
+                    return _taxes.MarriedFilingJointly(Income);
+                case "Y2":
+                    return _taxes.MarriedFilingSeperately(Income);
+                case "Z":
+                    return _taxes.HeadOfHousehold(Income);
+                default:
+                    return -1;
+            }
+        }
+    }
+}
---
+++ MockTaxesInterface/UnitTestMockTaxesInterface/Seminar4UnitTest.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MockTaxesInterface;
+
+namespace UnitTestMockTaxesInterface
+{
+    public class MockTaxes : ITaxes
+    {
+        public double Single(double income)
+        {
+            if (income == 9075)
+            {
+                return 907.5;
+            }
+            else
+                return 0;
+        }
+
+        public double MarriedFilingJointly(double income)
+        {
+            if (income == 73800)
+            {
+                return 10162.50;
+            }
+            else
+                return 0;
+        }
+
+        public double MarriedFilingSeperately(double income)
+        {
+            if (income == 74425)
+            {
+                return 14462.50;
+            }
+            else
+                return 0;
+        }
+
+        public double HeadOfHousehold(double income)
+        {
+            if (income == 206600)
+            {
+                return 48434.00;
+            }
+            else
+                return 0;
+        }
+    }
+
+        [TestClass]
+    public class TestOfMockTaxesInterface
+    {
+        [TestMethod]
+        public void TestOfMockSingle()
+        {
+            var taxes = new MockTaxes();
+            var taxDetermination = new TaxDetermination(taxes);
+
+            const string Schedule = "X";
+            const double Income = 9075;
+            const double ExpectedTax = 907.5;
+
+            var ActualTax = taxDetermination.ScheduleSort(Schedule, Income);
+
+            Assert.AreEqual(ExpectedTax, ActualTax);
+        }
+
+        [TestMethod]
+        public void TestOfMockMarriedFilingJointly()
+        {
+            var taxes = new MockTaxes();
+            var taxDetermination = new TaxDetermination(taxes);
+
+            const string Schedule = "y1";       //Also tests conversion to upper
+            const double Income = 73800;
+            const double ExpectedTax = 10162.50;
+
+            var ActualTax = taxDetermination.ScheduleSort(Schedule, Income);
+
+            Assert.AreEqual(ExpectedTax, ActualTax);
+        }
+
+        [TestMethod]
+        public void TestOfMockMarriedFilingSeperately()
+        {
+            var taxes = new MockTaxes();
+            var taxDetermination = new TaxDetermination(taxes);
+
+            const string Schedule = "y2";       //Also tests conversion to upper
+            const double Income = 74425;
+            const double ExpectedTax = 14462.50;
+
+            var ActualTax = taxDetermination.ScheduleSort(Schedule, Income);
+
+            Assert.AreEqual(ExpectedTax, ActualTax);
+        }
+
+        [TestMethod]
+        public void TestOfMockHeadOfHousehold()
+        {
+            var taxes = new MockTaxes();
+            var taxDetermination = new TaxDetermination(taxes);
+
+            const string Schedule = "Z";
+            const double Income = 206600;
+            const double ExpectedTax = 48434.00;
+
+            var ActualTax = taxDetermination.ScheduleSort(Schedule, Income);
+
+            Assert.AreEqual(ExpectedTax, ActualTax);
+        }
+    }
+}
